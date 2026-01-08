82e5f1b7f13d1940267ba906f68637074f71bfa5
ErodeesFleurs <862959461@qq.com>
2026-01-08 10:57:55 +0800

    update dconf for nemo default terminal

--- home/application/filebrowser/nemo.nix
+++ home/application/filebrowser/nemo.nix
@@ -10,11 +10,17 @@ in
 {
   options.homeModules.nemo = {
     enable = lib.mkEnableOption "Nemo file manager";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.nemo-with-extensions;
+      description = "Nemo file manager package";
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      (nemo-with-extensions.override {
+    home.packages = [
+      (cfg.package.override {
         extensions = with pkgs; [
           nemo-python
           nemo-fileroller
--- home/desktop/xdg.nix
+++ home/desktop/xdg.nix
@@ -12,6 +12,7 @@
     mimeApps = {
       enable = true;
       defaultApplicationPackages = [
+        config.homeModules.nemo.package
         config.programs.ghostty.package
         config.programs.zed-editor.package
         config.programs.firefox.package
@@ -22,6 +23,13 @@
     configFile = {
       "mimeapps.list".force = true;
       "uwsm/env".source = "${config.home.sessionVariablesPackage}/etc/profile.d/hm-session-vars.sh";
+    };
+
+    desktopEntries = {
+      nemo = {
+        name = "Nemo";
+        exec = "${pkgs.nemo-with-extensions}/bin/nemo";
+      };
     };
   };
 }
