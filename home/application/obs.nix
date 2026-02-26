7833c88700010973ace5608bbb532a0e3771af6a
ErodeesFleurs <862959461@qq.com>
2026-02-26 17:40:26 +0800

    add websocket plugin

--- home/application/obs.nix
+++ home/application/obs.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 
@@ -10,19 +11,14 @@ in
 {
   options.homeModules.obs = {
     enable = lib.mkEnableOption "OBS Studio";
-
-    plugins = lib.mkOption {
-      type = lib.types.listOf lib.types.package;
-      default = [
-      ];
-      description = "List of OBS Studio plugins to install";
-    };
   };
 
   config = lib.mkIf cfg.enable {
     programs.obs-studio = {
       enable = true;
-      plugins = cfg.plugins;
+      plugins = with pkgs; [
+        obs-studio-plugins.obs-websocket
+      ];
     };
   };
 }
