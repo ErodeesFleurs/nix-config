3237f270779b9d19f38260f5619c484a821b9c03
ErodeesFleurs <862959461@qq.com>
2026-08-17 15:51:39 +0800

    Update agent.nix

--- home/terminal/agent.nix
+++ home/terminal/agent.nix
@@ -14,6 +14,27 @@
         "opencode-skillful"
         "opencode-notificator"
       ];
+
+      provider = {
+        sudocode = {
+          npm = "@ai-sdk/openai-compatible";
+          "name" = "sudocode";
+          options = {
+            baseURL = "https://api.sudocode.chat/v1";
+          };
+          models = {
+            "gpt-5.6-sol" = {
+              "name" = "gpt-5.6-sol";
+            };
+            "gpt-5.6-terra" = {
+              "name" = "gpt-5.6-terra";
+            };
+            "gpt-5.6-luna" = {
+              "name" = "gpt-5.6-luna";
+            };
+          };
+        };
+      };
     };
   };
 }
