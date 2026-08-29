5dac091fab89888e944bdb090bf9f327f179c77d
ErodeesFleurs <862959461@qq.com>
2026-08-29 14:49:00 +0800

    chore: drop dead config.dae.age secret
    No module consumes it (dae config is rendered from module options);
    remove its secrets.nix rule too.

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,10 +3,6 @@ let
   dae = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJb9ab/4suVTwap6zlA2TMv0hfnBJ7WZ94a2lBcKN2Fd agenix_dae";
 in
 {
-  "config.dae.age".publicKeys = [
-    dae
-  ];
-
   "config.mihomo.yaml.age".publicKeys = [
     dae
   ];
